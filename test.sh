90fa55df5c607b64eaa56236e131c35604892874
rushi_543 <devlinux51@zengroup.co.in>
2026-09-15 11:20:08 +0530

    Add automated test to CI

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+output=$(./app.sh)
+
+if echo "$output" | grep -q "Hello From my CI/CD project" &&
+   echo "$output" | grep -q "Hello From RISHHIII"
+then 
+   echo "TEST PASSED"
+   exit 0
+else 
+   echo "TEST FAILED"
+   exit 1
+fi
